a8ea57943cd57a88a46d8cba23dba02edf5ccdb0
mintmending <198256056+mintmending@users.noreply.github.com>
2025-05-07 13:26:55 +0200

    Added functionality to sum overview

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 
 export const expenses = 
 [
-    {
-    date: "2025-05-06",
-    shop: "Rewe",
-    buyer: "Tom",
-    price: 50.00,
-    uuid: "a1b2c3d4-e5f6-7890-1234-567890abcdef" //example, change later
-    }
+    // {
+    // date: "2025-05-06",
+    // shop: "Rewe",
+    // buyer: "Tom",
+    // price: 50.00,
+    // uuid: "a1b2c3d4-e5f6-7890-1234-567890abcdef" //example, change later
+    // }
 ]
--- index.js
+++ index.js
@@ -1,11 +1,90 @@
 
 import { expenses } from "./data.js";
 
-const entryList = document.getElementById('entry-list');
+document.addEventListener("click", function(e){
+    if (e.target.id === "toggle-account"){
+        calculateAndRenderSum();
+    } 
+    
+    if (e.target.id === "save-entry-btn") {
+        let date = document.getElementById("date");
+        let shop = document.getElementById('shop');
+        let buyer = document.getElementById('buyer-select');
+        let price = document.getElementById('price');
+    
+        if(date.value && shop.value && buyer.value && price.value){
+    
+            let newExpense = {
+                date: date.value,
+                shop: shop.value,
+                buyer: buyer.value,
+                price: price.value,
+                uuid: "" // generate uuids here later (for integration of delete buttons)
+            }
+    
+            expenses.unshift(newExpense); // add sort by date instead of adding to the top?
+    
+            document.form1.reset();
+           
+            calculateAndRenderSum();
 
-renderEntries();
+            renderEntries();
+        }
+    }
+})
+
+function calculateAndRenderSum(){
+    let sum = 0;
+
+    const divSum = document.getElementById('div-sum');
+    const toggleAccount = document.getElementById("toggle-account");
+    let isToggleAccount = toggleAccount.checked;
+    const moneyText = document.getElementById("money-text");
+
+    expenses.forEach(function(expense){
+        if(expense.buyer === "Tom"){
+            sum += expense.price / 2;
+        } 
+        else if (expense.buyer === "Anna"){
+            sum -= expense.price / 2;
+        }
+        
+    })
+
+    if(isToggleAccount){ // true -> Anna is selected
+        if(sum >= 0){
+            divSum.classList.replace("money-getsback","money-owed");
+            toggleAccount.classList.replace("money-getsback","money-owed");
+            moneyText.classList.replace("money-getsback","money-owed");
+            moneyText.innerHTML = "owes";
+        } else { 
+            divSum.classList.replace("money-owed","money-getsback");
+            toggleAccount.classList.replace("money-owed","money-getsback");
+            moneyText.classList.replace("money-owed","money-getsback");
+            moneyText.innerHTML = "gets back";
+        }
+    } else { // Tom is selected
+        if(sum >= 0){ 
+            divSum.classList.replace("money-owed","money-getsback");
+            toggleAccount.classList.replace("money-owed","money-getsback");
+            moneyText.classList.replace("money-owed","money-getsback");
+            moneyText.innerHTML = "gets back";
+        } else {
+            divSum.classList.replace("money-getsback","money-owed");
+            toggleAccount.classList.replace("money-getsback","money-owed");
+            moneyText.classList.replace("money-getsback","money-owed");
+            moneyText.innerHTML = "owes";
+        }
+
+    }
+
+    divSum.innerHTML = Math.abs(sum) + "€";
+}
+
+
     
 function renderEntries(){
+    const entryList = document.getElementById('entry-list');
     let entryHTML = "";
 
     expenses.forEach(function(entry){
@@ -20,35 +99,10 @@ function renderEntries(){
             `
     })
 
-    // add a sort by date later?
+    // add a sort by date later? see event listener for entry btn
     entryList.innerHTML = entryHTML; 
 }
 
 
-// Code for entry-button here (refactor later into document event listener?)
-const saveEntryBtn = document.getElementById("save-entry-btn");
 
-saveEntryBtn.addEventListener("click", function(){
-
-    let date = document.getElementById("date");
-    let shop = document.getElementById('shop');
-    let buyer = document.getElementById('buyer-select');
-    let price = document.getElementById('price');
-
-    if(date.value && shop.value && buyer.value && price.value){
-
-        let newExpense = {
-            date: date.value,
-            shop: shop.value,
-            buyer: buyer.value,
-            price: price.value,
-            uuid: "" // generate uuids here later (for integration of delete buttons)
-        }
-
-        expenses.unshift(newExpense);
-
-        document.form1.reset();
-        renderEntries();
-    }
-})
 
